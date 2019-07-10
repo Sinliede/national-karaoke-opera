350b897909e7cc95d944501c3b3f62159dbe6211
zhaitianyu <zhaitianyu@5dgravity.com>
2019-07-10 09:44:03 +0800

    first commit

---
+++ src/main/java/com/sinliede/opera/application/Application.java
@@ -0,0 +1,14 @@
+package com.sinliede.opera.application;
+
+/**
+ * @author sinliede
+ * @date 19-7-9 下午5:55
+ * @since
+ */
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
